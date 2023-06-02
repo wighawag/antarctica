0403b3c055384fbe48ab55f56c7d52ed7957ff92
wighawag <wighawag@gmail.com>
2023-06-02 16:05:08 +0100

    removing graphql mutations

--- antartica-indexer/src/schema/resolvers/blocks.ts
+++ antartica-indexer/src/schema/resolvers/blocks.ts
@@ -23,60 +23,60 @@ builder.queryField('block', (t) =>
 	})
 );
 
-builder.mutationField('create_block', (t) =>
-	t.field({
-		type: BlockObjectType,
-		args: {
-			input: t.arg({
-				type: BlockObjectInput,
-				required: true,
-			}),
-		},
-		resolve: async (root, args, ctx) => {
-			return await ctx.db
-				.insertInto('blocks')
-				.values({
-					hash: args.input.hash,
-					number: args.input.number,
-				})
-				.returningAll()
-				.executeTakeFirstOrThrow();
-		},
-	})
-);
+// builder.mutationField('create_block', (t) =>
+// 	t.field({
+// 		type: BlockObjectType,
+// 		args: {
+// 			input: t.arg({
+// 				type: BlockObjectInput,
+// 				required: true,
+// 			}),
+// 		},
+// 		resolve: async (root, args, ctx) => {
+// 			return await ctx.db
+// 				.insertInto('blocks')
+// 				.values({
+// 					hash: args.input.hash,
+// 					number: args.input.number,
+// 				})
+// 				.returningAll()
+// 				.executeTakeFirstOrThrow();
+// 		},
+// 	})
+// );
 
-builder.mutationField('update_block', (t) =>
-	t.field({
-		type: BlockObjectType,
-		args: {
-			input: t.arg({
-				type: BlockObjectInput,
-				required: true,
-			}),
-		},
-		resolve: async (root, args, ctx) => {
-			const data = {
-				hash: args.input.hash,
-				number: args.input.number,
-			};
-			return await ctx.db
-				.insertInto('blocks')
-				.values(data)
-				.onConflict((oc) => oc.column('hash').doUpdateSet(data))
-				.returningAll()
-				.executeTakeFirstOrThrow();
-		},
-	})
-);
+// builder.mutationField('update_block', (t) =>
+// 	t.field({
+// 		type: BlockObjectType,
+// 		args: {
+// 			input: t.arg({
+// 				type: BlockObjectInput,
+// 				required: true,
+// 			}),
+// 		},
+// 		resolve: async (root, args, ctx) => {
+// 			const data = {
+// 				hash: args.input.hash,
+// 				number: args.input.number,
+// 			};
+// 			return await ctx.db
+// 				.insertInto('blocks')
+// 				.values(data)
+// 				.onConflict((oc) => oc.column('hash').doUpdateSet(data))
+// 				.returningAll()
+// 				.executeTakeFirstOrThrow();
+// 		},
+// 	})
+// );
 
-builder.mutationField('remove_block', (t) =>
-	t.field({
-		type: BlockObjectType,
-		args: {
-			hash: t.arg.string({required: true}),
-		},
-		resolve: async (root, args, ctx) => {
-			return await ctx.db.deleteFrom('blocks').where('hash', '=', args.hash).returningAll().executeTakeFirstOrThrow();
-		},
-	})
-);
+// builder.mutationField('remove_block', (t) =>
+// 	t.field({
+// 		type: BlockObjectType,
+// 		args: {
+// 			hash: t.arg.string({required: true}),
+// 		},
+// 		resolve: async (root, args, ctx) => {
+// 			return await ctx.db.deleteFrom('blocks').where('hash', '=', args.hash).returningAll().executeTakeFirstOrThrow();
+// 		},
+// 	})
+// );
--- antartica-indexer/src/schema/resolvers/transactions.ts
+++ antartica-indexer/src/schema/resolvers/transactions.ts
@@ -27,66 +27,66 @@ builder.queryField('transaction', (t) =>
 	})
 );
 
-builder.mutationField('create_transaction', (t) =>
-	t.field({
-		type: TransactionObjectType,
-		args: {
-			input: t.arg({
-				type: TransactionObjectInput,
-				required: true,
-			}),
-		},
-		resolve: async (root, args, ctx) => {
-			return await ctx.db
-				.insertInto('transactions')
-				.values({
-					hash: args.input.hash,
-					from: args.input.from,
-					to: args.input.to,
-				})
-				.returningAll()
-				.executeTakeFirstOrThrow();
-		},
-	})
-);
+// builder.mutationField('create_transaction', (t) =>
+// 	t.field({
+// 		type: TransactionObjectType,
+// 		args: {
+// 			input: t.arg({
+// 				type: TransactionObjectInput,
+// 				required: true,
+// 			}),
+// 		},
+// 		resolve: async (root, args, ctx) => {
+// 			return await ctx.db
+// 				.insertInto('transactions')
+// 				.values({
+// 					hash: args.input.hash,
+// 					from: args.input.from,
+// 					to: args.input.to,
+// 				})
+// 				.returningAll()
+// 				.executeTakeFirstOrThrow();
+// 		},
+// 	})
+// );
 
-builder.mutationField('update_transaction', (t) =>
-	t.field({
-		type: TransactionObjectType,
-		args: {
-			input: t.arg({
-				type: TransactionObjectInput,
-				required: true,
-			}),
-		},
-		resolve: async (root, args, ctx) => {
-			const data = {
-				hash: args.input.hash,
-				from: args.input.from,
-				to: args.input.to,
-			};
-			return await ctx.db
-				.insertInto('transactions')
-				.values(data)
-				.onConflict((oc) => oc.column('hash').doUpdateSet(data))
-				.returningAll()
-				.executeTakeFirstOrThrow();
-		},
-	})
-);
+// builder.mutationField('update_transaction', (t) =>
+// 	t.field({
+// 		type: TransactionObjectType,
+// 		args: {
+// 			input: t.arg({
+// 				type: TransactionObjectInput,
+// 				required: true,
+// 			}),
+// 		},
+// 		resolve: async (root, args, ctx) => {
+// 			const data = {
+// 				hash: args.input.hash,
+// 				from: args.input.from,
+// 				to: args.input.to,
+// 			};
+// 			return await ctx.db
+// 				.insertInto('transactions')
+// 				.values(data)
+// 				.onConflict((oc) => oc.column('hash').doUpdateSet(data))
+// 				.returningAll()
+// 				.executeTakeFirstOrThrow();
+// 		},
+// 	})
+// );
 
-builder.mutationField('remove_transaction', (t) =>
-	t.field({
-		type: TransactionObjectType,
-		args: {
-			hash: t.arg.string({required: true}),
-		},
-		resolve: async (root, args, ctx) => {
-			return await ctx.db
-				.deleteFrom('transactions')
-				.where('hash', '=', args.hash)
-				.returningAll()
-				.executeTakeFirstOrThrow();
-		},
-	})
-);
+// builder.mutationField('remove_transaction', (t) =>
+// 	t.field({
+// 		type: TransactionObjectType,
+// 		args: {
+// 			hash: t.arg.string({required: true}),
+// 		},
+// 		resolve: async (root, args, ctx) => {
+// 			return await ctx.db
+// 				.deleteFrom('transactions')
+// 				.where('hash', '=', args.hash)
+// 				.returningAll()
+// 				.executeTakeFirstOrThrow();
+// 		},
+// 	})
+// );
